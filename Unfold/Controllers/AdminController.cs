a7963b832314bec0c2b2af63825e53fbbce2cf69
BeyzaKayraklik <76918032+BeyzaKayraklik@users.noreply.github.com>
2022-02-02 13:16:30 +0300

    wEB

---
+++ Unfold/Controllers/AdminController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Unfold.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Home()
+        {
+            return View();
+        }
+        public IActionResult Main()
+        {
+            return View();
+        }
+        public IActionResult Portfolio()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Services()
+        {
+            return View();
+        }
+        public IActionResult Skills()
+        {
+            return View();
+        }
+        public IActionResult Categories()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult Social()
+        {
+            return View();
+        }
+        public IActionResult Messages()
+        {
+            return View();
+        }
+    }
+}
---
+++ Unfold/Controllers/EditController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Unfold.Controllers
+{
+    public class EditController : Controller
+    {
+        public IActionResult Main()
+        {
+            return View();
+        }
+
+        public IActionResult Home()
+        {
+            return View();
+        }
+        public IActionResult Portfolio()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Services()
+        {
+            return View();
+        }
+        public IActionResult Skills()
+        {
+            return View();
+        }
+        public IActionResult Categories()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult Social()
+        {
+            return View();
+        }
+       
+    }
+}
